09471c1277595690e793d867c76afdaba030541d
Vincent Jaquet-Hertgen <vincent.hertgen@gmail.com>
2026-05-27 15:57:02 +0200

    Ajout de la classe Belman Ford et de la classe calculer

--- src/main/java/BellmanFord.java
+++ src/main/java/BellmanFord.java
@@ -1,6 +1,33 @@
 public class BellmanFord {
-    private Graphe g;
-    public Valeurs calculer(){
-        return new Valeurs();
+    public Valeurs calculer(GrapheListe g, String depart){
+
+        Valeurs vieux = new Valeurs();
+        for(String noeud : g.listeNoeuds()){
+            if (noeud.equals(depart)){
+                vieux.ajouterValeur(noeud, 0.0, null);
+            }
+            vieux.ajouterValeur(noeud, Double.MAX_VALUE, null);
+        }
+
+        boolean changement = true;
+        while(changement) {
+            changement = false;
+            for (String noeud : g.listeNoeuds()) {
+                double coutMin = Double.MAX_VALUE;
+                String noeudMin = "";
+                if (vieux.getValeur(noeud) != Double.MAX_VALUE) {
+                    for (Arc arc : g.suivants(noeud).getArcs()) {
+                        if (arc.getCout() + vieux.getValeur(noeud) < coutMin) {
+                            coutMin = arc.getCout()+vieux.getValeur(noeud);
+                            noeudMin = arc.getDestination();
+                        }
+                    }
+                    vieux.setValeur(noeudMin, coutMin);
+                    vieux.setParent(noeud, noeudMin);
+                    changement = true;
+                }
+            }
+        }
+        return vieux;
     }
 }
