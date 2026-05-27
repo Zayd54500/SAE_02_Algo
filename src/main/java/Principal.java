c30229834a2696fdbd6e8c6ea3b937b60e9af018
Andaloussi212 <zayd.benz.pro@proton.me>
2026-05-27 16:45:22 +0200

    Ajout du code de Dijkstra + test dans la classe Principale

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,13 +1,17 @@
 public class Principal {
     public static void main(String[] args) {
         GrapheListe graphe = new GrapheListe();
-        graphe.ajouterArc("A", "B", 12);
-        graphe.ajouterArc("A", "D", 87);
-        graphe.ajouterArc("B", "E", 11);
-        graphe.ajouterArc("C", "A", 19);
-        graphe.ajouterArc("D", "B", 23);
-        graphe.ajouterArc("D", "C", 10);
-        graphe.ajouterArc("E", "D", 43);
+        graphe.ajouterArc("A", "B", 12.0);
+        graphe.ajouterArc("A", "D", 87.0);
+        graphe.ajouterArc("B", "E", 11.0);
+        graphe.ajouterArc("C", "A", 19.0);
+        graphe.ajouterArc("D", "B", 23.0);
+        graphe.ajouterArc("D", "C", 10.0);
+        graphe.ajouterArc("E", "D", 43.0);
         System.out.println(graphe);
+
+        Dijkstra dijkstra = new Dijkstra();
+        Valeurs resultat = dijkstra.resoudre(graphe, "A");
+        System.out.println(resultat);
     }
 }
