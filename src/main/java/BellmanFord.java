e6e11de49b7005cc92ea1e100bee47fc3eafc3a1
Andaloussi212 <zayd.benz.pro@proton.me>
2026-05-30 01:41:53 +0200

    Finnalisation de Bellman-Ford

--- src/main/java/BellmanFord.java
+++ src/main/java/BellmanFord.java
@@ -8,8 +8,25 @@ public class BellmanFord {
                 valeur.ajouterValeur(noeud, 0.0, null);
             } else {
                 valeur.ajouterValeur(noeud, Double.MAX_VALUE, null);
-
             }
         }
+        for (int i = 0; i < graphe.listeNoeuds().size() - 1; i++) {
+            for (int j = 0; j < graphe.listeNoeuds().size(); j++) {
+                String noeud = graphe.listeNoeuds().get(j);
+                Arcs arcs = graphe.suivants(noeud);
+                for (int k = 0; k < arcs.getArcs().size(); k++) {
+                    Arc arc = arcs.getArcs().get(k);
+                    String destination = arc.getDestination();
+                    double cout = arc.getCout();
+                    double nouvelleValeur = valeur.getValeur(noeud) + cout;
+
+                    if (nouvelleValeur < valeur.getValeur(destination)) {
+                        valeur.setValeur(destination,  nouvelleValeur);
+                        valeur.setParent(destination, noeud);
+                    }
+                }
+            }
+        }
+        return valeur;
     }
 }
