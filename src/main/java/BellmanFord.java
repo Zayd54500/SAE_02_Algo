7608bb876a191fc55327a47bc46c915e78f5c5b1
Andaloussi212 <zayd.benz.pro@proton.me>
2026-05-30 01:22:25 +0200

    Réecriture de Bellman-Ford (Initialisation seulement)

--- src/main/java/BellmanFord.java
+++ src/main/java/BellmanFord.java
@@ -1,33 +1,14 @@
 public class BellmanFord {
-    public Valeurs calculer(GrapheListe g, String depart){
+    public Valeurs calculer(Graphe graphe, String depart) {
+        Valeurs valeur = new Valeurs();
+        for (int i = 0; i < graphe.listeNoeuds().size(); i++) {
+            String noeud = graphe.listeNoeuds().get(i);
 
-        Valeurs vieux = new Valeurs();
-        for(String noeud : g.listeNoeuds()){
-            if (noeud.equals(depart)){
-                vieux.ajouterValeur(noeud, 0.0, null);
-            }
-            vieux.ajouterValeur(noeud, Double.MAX_VALUE, null);
-        }
-
-        boolean changement = true;
-        while(changement) {
-            changement = false;
-            for (String noeud : g.listeNoeuds()) {
-                double coutMin = Double.MAX_VALUE;
-                String noeudMin = "";
-                if (vieux.getValeur(noeud) != Double.MAX_VALUE) {
-                    for (Arc arc : g.suivants(noeud).getArcs()) {
-                        if (arc.getCout() + vieux.getValeur(noeud) < coutMin) {
-                            coutMin = arc.getCout()+vieux.getValeur(noeud);
-                            noeudMin = arc.getDestination();
-                        }
-                    }
-                    vieux.setValeur(noeudMin, coutMin);
-                    vieux.setParent(noeudMin, noeud);
-                    changement = true;
-                }
+            if (noeud.equals(depart)) {
+                valeur.ajouterValeur(noeud, 0.0, null);
+            } else {
+                valeur.ajouterValeur(noeud, Double.MAX_VALUE, null);
             }
         }
-        return vieux;
     }
 }
